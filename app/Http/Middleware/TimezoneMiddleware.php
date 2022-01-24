da60f46c81bcfd95dc2ea1f7711ce8e2b73464ac
artemkjv <kavkalo1@gmail.com>
2022-01-24 11:48:30 +0200

    fixed getting wrong client ip for timezone

--- app/Http/Middleware/TimezoneMiddleware.php
+++ app/Http/Middleware/TimezoneMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Libraries\Helpers\IPHelper;
 use App\Libraries\Helpers\TimezoneHelper;
 use Closure;
 use Illuminate\Http\Request;
@@ -18,7 +19,8 @@ class TimezoneMiddleware
     public function handle(Request $request, Closure $next)
     {
         if(!session()->has('timezone')){
-            $ip = request()->getClientIp();
+            $ipHelper = IPHelper::instance();
+            $ip = $ipHelper->ip();
             $timezoneHelper = TimezoneHelper::instance();
             session()->put('timezone', $timezoneHelper->getTimezoneFromIp($ip));
         }
