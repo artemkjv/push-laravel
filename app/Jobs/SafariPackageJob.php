fbe10d90813f4805488e23d4f3c63074365f9748
kavka <kavkalo1@gmail.com>
2022-06-22 22:54:23 +0300

    implemented creating push packages

--- app/Jobs/SafariPackageJob.php
+++ app/Jobs/SafariPackageJob.php
@@ -3,13 +3,18 @@
 namespace App\Jobs;
 
 use App\Models\App;
+use App\Services\Exceptions\CertificateException;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+use ZipArchive;
 
 class SafariPackageJob implements ShouldQueue
 {
@@ -23,6 +28,16 @@ class SafariPackageJob implements ShouldQueue
         16,
         32,
         128
+    ];
+
+    public const RAW_FILES = [
+        'icon.iconset/icon_16x16.png',
+        'icon.iconset/icon_16x16@2x.png',
+        'icon.iconset/icon_32x32.png',
+        'icon.iconset/icon_32x32@2x.png',
+        'icon.iconset/icon_128x128.png',
+        'icon.iconset/icon_128x128@2x.png',
+        'website.json'
     ];
 
     /**
@@ -46,10 +61,34 @@ class SafariPackageJob implements ShouldQueue
     {
         $fullPath = \Storage::path($this->certificatePath);
         $certificatePassword = $this->app->web_private_key;
-        $webIcon = \Storage::get($this->app->web_icon);
+        $webIcon = \Storage::path($this->app->web_icon);
         $packageDir = "package-pushes/{$this->app->user_id}/{$this->app->id}/";
         $this->createIcons($webIcon, $packageDir);
         $this->createWebsiteConfig($packageDir);
+        $this->createManifest($packageDir, 2, self::RAW_FILES);
+        $this->createSignature($packageDir, $fullPath, $certificatePassword);
+        $this->packageRawData($packageDir, self::RAW_FILES);
+    }
+
+    function packageRawData($packageDir, $rawFiles) {
+        $zipPath = Storage::path("{$packageDir}Package_Push.zip");
+        echo $zipPath;
+        // Package files as a zip file
+        $zip = new ZipArchive();
+        if (!$zip->open($zipPath, ZIPARCHIVE::CREATE)) {
+            throw new Exception('Could not create ' . $zipPath);
+            return;
+        }
+
+        //$raw_files = raw_files();
+        $rawFiles[] = 'manifest.json';
+        $rawFiles[] = 'signature';
+        foreach ($rawFiles as $rawFile) {
+            $zip->addFile(Storage::path("$packageDir$rawFile"), $rawFile);
+        }
+
+        $zip->close();
+        return $zipPath;
     }
 
     private function createWebsiteConfig($packageDir) {
@@ -61,8 +100,33 @@ class SafariPackageJob implements ShouldQueue
             'authenticationToken' => '19f8d7a6e9fb8a7f6d9330dabe',
             'webServiceUrl' => config('app.url')
         ];
-        $websiteConfigJson = json_encode($websiteConfig);
+        $websiteConfigJson = json_encode($websiteConfig, JSON_UNESCAPED_SLASHES);
         \Storage::put($packageDir . 'website.json', $websiteConfigJson);
+    }
+
+    function createSignature($packageDir, $certPath, $certPassword) {
+        // Load the push notification certificate
+        $pkcs12 = file_get_contents($certPath);
+        $certs = [];
+        if(!openssl_pkcs12_read($pkcs12, $certs, $certPassword)) {
+            return;
+        }
+
+        $signaturePath = Storage::path("{$packageDir}signature");
+
+        // Sign the manifest.json file with the private key from the certificate
+        $certData = openssl_x509_read($certs['cert']);
+        $privateKey = openssl_pkey_get_private($certs['pkey'], $certPassword);
+        openssl_pkcs7_sign(Storage::path("$packageDir/manifest.json"), $signaturePath, $certData, $privateKey, [], PKCS7_BINARY | PKCS7_DETACHED);
+
+        // Convert the signature from PEM to DER
+        $signaturePem = file_get_contents($signaturePath);
+        $matches = array();
+        if (!preg_match('~Content-Disposition:[^\n]+\s*?([A-Za-z0-9+=/\r\n]+)\s*?-----~', $signaturePem, $matches)) {
+            return;
+        }
+        $signatureDer = base64_decode($matches[1]);
+        file_put_contents($signaturePath, $signatureDer);
     }
 
     private function createIcons($webIcon, $packageDir) {
@@ -77,4 +141,26 @@ class SafariPackageJob implements ShouldQueue
             })->save(storage_path('app/' . $imageDir . "/icon_{$imageSize}x{$imageSize}@2x.png"));
         }
     }
+
+    private function createManifest($packageDir, $packageVersion, $rawFiles) {
+        $manifestData = array();
+        foreach ($rawFiles as $rawFile) {
+            $fileContents = Storage::get("$packageDir$rawFile");
+            if ($packageVersion === 1) {
+                $manifestData[$rawFile] = sha1($fileContents);
+            } else if ($packageVersion === 2) {
+                $hashType = 'sha512';
+                $manifestData[$rawFile] = array(
+                    'hashType' => $hashType,
+                    'hashValue' => hash($hashType, $fileContents),
+                );
+            } else {
+                throw new CertificateException('Invalid push package version.');
+            }
+        }
+        Storage::put("{$packageDir}manifest.json", json_encode((object)$manifestData));
+    }
+
+
+
 }
