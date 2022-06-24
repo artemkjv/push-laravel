293d7f9b37690ff341356858434ec6b1964e1ed3
kavka <kavkalo1@gmail.com>
2022-06-24 16:21:52 +0300

    made some changes

--- public/assets/js/webpush.js
+++ public/assets/js/webpush.js
@@ -1,84 +1,131 @@
 const BASE_URL = 'https://push.devonics.pro'
 
 function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
 
-class Api{
-    getApp(appId){
+class Api {
+    getApp(appId) {
         return fetch(`${BASE_URL}/api/apps/${appId}/show`)
             .then(response => response.json())
-            .then(data => data.data.sender_id)
+            .then(data => data.data)
     }
-    ipLookUp(){
+
+    ipLookUp() {
         return fetch('https://pro.ip-api.com/json/?key=I9ShYw6mCZh58E4')
             .then(response => response.json())
     }
-    subscribe(pushUser){
+
+    subscribe(pushUser) {
         return fetch(`${BASE_URL}/api/push-users`, {
             method: 'POST',
             body: JSON.stringify(pushUser)
         })
     }
-    session(currentToken){
+
+    session(currentToken) {
         return fetch(`${BASE_URL}/api/push-users/${currentToken}/session`)
     }
 }
 
-class Cookie{
-    setCookie(name, value, days){
+class Cookie {
+    setCookie(name, value, days) {
         let expires = ''
-        if(days){
+        if (days) {
             const date = new Date()
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
             expires = `; expires=${date.toGMTString()}`
         }
         document.cookie = `${name}=${value + expires}; path=/`
     }
-    getCookie(name){
+
+    getCookie(name) {
         let nameEQ = `${name}=`
         let ca = document.cookie.split(';')
-        for(let i = 0; i < ca.length; i++) {
+        for (let i = 0; i < ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) === ' ') c = c.substring(1, c.length);
             if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
         }
         return null;
     }
-    deleteCookie(name){
+
+    deleteCookie(name) {
         this.setCookie(name, '', -1)
     }
 }
-var DevonicsPush = {
+
+const SafariPush = {
     api: new Api(),
     cookie: new Cookie(),
-    saveSessionCookie(currentToken){
-        console.log(currentToken)
-        let firstVisit = this.cookie.getCookie('firstVisit')
-        if(firstVisit === 'false') return
-        this.api.session(currentToken)
-        this.cookie.setCookie('firstVisit', false)
+    initialize(appId) {
+        SafariPush.api.getApp(appId)
+            .then(data => {
+                this.appId = appId
+                this.safari_web_id = data.safari_web_id
+                SafariPush.requestPermission()
+            })
     },
-    isTokenSaved(currentToken){
+    requestPermission() {
+        let permissionData = window.safari.pushNotification.permission(this.safari_web_id);
+        if (permissionData.permission === 'default') {
+            window.safari.pushNotification.requestPermission(
+                'https://push.devonics.pro',
+                this.safari_web_id,
+                {},
+                SafariPush.subscribe
+            );
+        }
+    },
+    subscribe() {
+        let permissionData = window.safari.pushNotification.permission(this.safari_web_id);
+        if (permissionData.permission === 'granted') {
+            console.log(permissionData.deviceToken, 'YEAH!');
+            SafariPush.saveToken(permissionData.deviceToken);
+        }
+    },
+    saveToken(deviceToken) {
+        SafariPush.api.ipLookUp()
+            .then(response => {
+                SafariPush.api.subscribe({
+                    registration_id: deviceToken,
+                    app_id: this.app_id,
+                    country: response.countryCode,
+                    platform_id: 3,
+                    timezone: response.timezone,
+                    language: navigator.language
+                        .substring(0, 2)
+                        .toUpperCase(),
+                    uuid: uuidv4()
+                })
+            })
+    }
+}
+
+const WebPush = {
+    api: new Api(),
+    cookie: new Cookie(),
+    isTokenSaved(currentToken) {
         return window.localStorage.getItem('sentFirebaseMessagingToken') === currentToken;
     },
-    saveToken(currentToken, appId){
-        if (!this.isTokenSaved(currentToken)) {
+    saveToken(currentToken, appId) {
+        if (!WebPush.isTokenSaved(currentToken)) {
             console.log('Sending a token to the server...');
-            this.api.ipLookUp()
+            WebPush.api.ipLookUp()
                 .then(response => {
-                    this.api.subscribe({
+                    WebPush.api.subscribe({
                         registration_id: currentToken,
                         app_id: appId,
                         country: response.countryCode,
                         platform_id: 3,
                         timezone: response.timezone,
                         language: navigator.language
-                            .substring(0,2)
+                            .substring(0, 2)
                             .toUpperCase(),
+                        is_safari: true,
                         uuid: uuidv4()
                     })
                         .then(() => {
@@ -89,17 +136,17 @@ var DevonicsPush = {
                         })
                 })
         } else {
-            this.saveSessionCookie(currentToken);
+            DevonicsPush.saveSessionCookie(currentToken);
             console.log('The token has already been sent to the server.');
         }
     },
-    subscribe(messaging, appId){
+    subscribe(messaging, appId) {
         messaging.requestPermission()
             .then(function () {
                 messaging.getToken()
                     .then(function (currentToken) {
                         if (currentToken) {
-                            DevonicsPush.saveToken(currentToken, appId);
+                            WebPush.saveToken(currentToken, appId);
                         } else {
                             console.warn('Failed to get token.');
                         }
@@ -112,42 +159,61 @@ var DevonicsPush = {
                 console.warn('Failed to get permission to show notifications.', err);
             });
     },
-    onTokenRefresh(messaging){
+    onTokenRefresh(messaging) {
         messaging.getToken()
-            .then(function(refreshedToken) {
+            .then(function (refreshedToken) {
                 console.log('Token refreshed');
-                this.saveToken(refreshedToken);
+                WebPush.saveToken(refreshedToken);
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.error('Unable to retrieve refreshed token', error);
             });
     },
-    onMessage(payload){
+    onMessage(payload) {
         console.log('Message received', payload);
         navigator.serviceWorker.register('/firebase-messaging-sw.js');
-        Notification.requestPermission(function(permission) {
+        Notification.requestPermission(function (permission) {
             if (permission === 'granted') {
-                navigator.serviceWorker.ready.then(function(registration) {
+                navigator.serviceWorker.ready.then(function (registration) {
                     payload.data.data = JSON.parse(JSON.stringify(payload.data));
                     registration.showNotification(payload.data.title, payload.data);
-                }).catch(function(error) {
+                }).catch(function (error) {
                     console.error('ServiceWorker registration failed', error);
                 });
             }
         });
     },
-    initialize(appId){
-        this.api.getApp(appId)
-            .then(senderId => {
+    initialize(appId) {
+        WebPush.api.getApp(appId)
+            .then(data => {
                 firebase.initializeApp({
-                    messagingSenderId: senderId,
+                    messagingSenderId: data.sender_id,
                 });
                 if ('Notification' in window) {
                     const messaging = firebase.messaging();
-                    messaging.onTokenRefresh(this.onTokenRefresh)
-                    messaging.onMessage(this.onMessage)
-                    this.subscribe(messaging, appId);
+                    messaging.onTokenRefresh(WebPush.onTokenRefresh)
+                    messaging.onMessage(WebPush.onMessage)
+                    WebPush.subscribe(messaging, appId);
                 }
             })
+    }
+}
+
+var DevonicsPush = {
+    api: new Api(),
+    cookie: new Cookie(),
+    saveSessionCookie(currentToken) {
+        console.log(currentToken)
+        let firstVisit = DevonicsPush.cookie.getCookie('firstVisit')
+        if (firstVisit === 'false') return
+        DevonicsPush.api.session(currentToken)
+        DevonicsPush.cookie.setCookie('firstVisit', false)
+    },
+    initialize(appId) {
+        if ('safari' in window && 'pushNotification' in window.safari) {
+            SafariPush.initialize(appId)
+        } else {
+            WebPush.initialize(appId);
+        }
     }
 };
