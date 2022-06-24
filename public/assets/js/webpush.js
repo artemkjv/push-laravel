6ed4bca996f7b81777d8d30258e913d71c54a8ea
kavka <kavkalo1@gmail.com>
2022-06-24 15:22:47 +0300

    made some changes

--- public/assets/js/webpush.js
+++ public/assets/js/webpush.js
@@ -64,24 +64,24 @@ const SafariPush = {
     initialize(appId) {
         SafariPush.api.getApp(appId)
             .then(data => {
-                SafariPush.appId = appId
-                SafariPush.safari_web_id = safari_web_id
+                var appId = appId
+                var safari_web_id = safari_web_id
                 SafariPush.requestPermission()
             })
     },
     requestPermission() {
-        let permissionData = window.safari.pushNotification.permission(SafariPush.safari_web_id);
+        let permissionData = window.safari.pushNotification.permission(window.safari_web_id);
         if (permissionData.permission === 'default') {
             window.safari.pushNotification.requestPermission(
                 'https://push.devonics.pro',
-                SafariPush.safari_web_id,
+                window.safari_web_id,
                 {},
                 SafariPush.subscribe
             );
         }
     },
     subscribe() {
-        let permissionData = window.safari.pushNotification.permission(SafariPush.safari_web_id);
+        let permissionData = window.safari.pushNotification.permission(window.safari_web_id);
         if (permissionData.permission === 'granted') {
             console.log(permissionData.deviceToken, 'YEAH!');
             SafariPush.saveToken(permissionData.deviceToken);
@@ -92,7 +92,7 @@ const SafariPush = {
             .then(response => {
                 SafariPush.api.subscribe({
                     registration_id: deviceToken,
-                    app_id: SafariPush.app_id,
+                    app_id: window.app_id,
                     country: response.countryCode,
                     platform_id: 3,
                     timezone: response.timezone,
